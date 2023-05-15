7fe4b877c1672f64f0ea11ca64a46eeb61e7d7aa
Jazmin Ivy <princessivy07@yahoo.com>
2023-05-14 21:37:20 -0500

    Added a string representation of the Track model in the backend

--- backend/database/models.py
+++ backend/database/models.py
@@ -45,6 +45,9 @@ class Track(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship("User")
 
+    def __repr__(self):
+        return f"{self.title} {self.time} {self.bpm} {self.genre} {self.release_date} {self.price}"
+
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
