583f697561c561a754e04a93238f7f3e789bc297
Jazmin Ivy <princessivy07@yahoo.com>
2023-06-05 16:31:54 -0500

    Adding more shopping cart code to the backend

--- backend/app.py
+++ backend/app.py
@@ -70,110 +70,110 @@ def create_routes():
     return api
 
 # @app.route('/add', methods=['POST'])
-# def add_product_to_cart():
-# 	cursor = None
-# 	try:
-# 		_quantity = int(request.form['quantity'])
-# 		_code = request.form['code']
-# 		# validate the received values
-# 		if _quantity and _code and request.method == 'POST':
-# 			conn = db.connect()
-# 			cursor = conn.cursor(db.cursors.DictCursor)
-# 			cursor.execute("SELECT * FROM product WHERE code=%s", _code)
-# 			row = cursor.fetchone()
+def add_product_to_cart():
+	cursor = None
+	try:
+		quantity = int(request.form['quantity'])
+		code = request.form['code']
+		# validate the received values
+		if quantity and code and request.method == 'POST':
+			conn = db.connect()
+			cursor = conn.cursor(db.cursors.DictCursor)
+			cursor.execute("SELECT * FROM product WHERE code=%s", code)
+			row = cursor.fetchone()
 			
-# 			itemArray = { row['code'] : {'name' : row['name'], 'code' : row['code'], 'quantity' : _quantity, 'price' : row['price'], 'image' : row['image'], 'total_price': _quantity * row['price']}}
+			itemArray = { row['code'] : {'name' : row['name'], 'code' : row['code'], 'quantity' : quantity, 'price' : row['price'], 'image' : row['image'], 'total_price': quantity * row['price']}}
 			
-# 			all_total_price = 0
-# 			all_total_quantity = 0
+			all_total_price = 0
+			all_total_quantity = 0
 			
-# 			session.modified = True
-# 			if 'cart_item' in session:
-# 				if row['code'] in session['cart_item']:
-# 					for key, value in session['cart_item'].items():
-# 						if row['code'] == key:
-# 							#session.modified = True
-# 							#if session['cart_item'][key]['quantity'] is not None:
-# 							#	session['cart_item'][key]['quantity'] = 0
-# 							old_quantity = session['cart_item'][key]['quantity']
-# 							total_quantity = old_quantity + _quantity
-# 							session['cart_item'][key]['quantity'] = total_quantity
-# 							session['cart_item'][key]['total_price'] = total_quantity * row['price']
-# 				else:
-# 					session['cart_item'] = array_merge(session['cart_item'], itemArray)
+			session.modified = True
+			if 'cart_item' in session:
+				if row['code'] in session['cart_item']:
+					for key, value in session['cart_item'].items():
+						if row['code'] == key:
+							#session.modified = True
+							#if session['cart_item'][key]['quantity'] is not None:
+							#	session['cart_item'][key]['quantity'] = 0
+							old_quantity = session['cart_item'][key]['quantity']
+							total_quantity = old_quantity + quantity
+							session['cart_item'][key]['quantity'] = total_quantity
+							session['cart_item'][key]['total_price'] = total_quantity * row['price']
+				else:
+					session['cart_item'] = array_merge(session['cart_item'], itemArray)
 
-# 				for key, value in session['cart_item'].items():
-# 					individual_quantity = int(session['cart_item'][key]['quantity'])
-# 					individual_price = float(session['cart_item'][key]['total_price'])
-# 					all_total_quantity = all_total_quantity + individual_quantity
-# 					all_total_price = all_total_price + individual_price
-# 			else:
-# 				session['cart_item'] = itemArray
-# 				all_total_quantity = all_total_quantity + _quantity
-# 				all_total_price = all_total_price + _quantity * row['price']
+				for key, value in session['cart_item'].items():
+					individual_quantity = int(session['cart_item'][key]['quantity'])
+					individual_price = float(session['cart_item'][key]['total_price'])
+					all_total_quantity = all_total_quantity + individual_quantity
+					all_total_price = all_total_price + individual_price
+			else:
+				session['cart_item'] = itemArray
+				all_total_quantity = all_total_quantity + quantity
+				all_total_price = all_total_price + quantity * row['price']
 			
-# 			session['all_total_quantity'] = all_total_quantity
-# 			session['all_total_price'] = all_total_price
+			session['all_total_quantity'] = all_total_quantity
+			session['all_total_price'] = all_total_price
 			
-# 			return redirect(url_for('.products'))
-# 		else:			
-# 			return 'Error while adding item to cart'
-# 	except Exception as e:
-# 		print(e)
-# 	finally:
-# 		cursor.close() 
-# 		conn.close()
+			return redirect(url_for('.products'))
+		else:			
+			return 'Error while adding item to cart'
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
 		
-# @app.route('/')
-# def products():
-# 	try:
-# 		conn = db.connect()
-# 		cursor = conn.cursor(db.cursors.DictCursor)
-# 		cursor.execute("SELECT * FROM product")
-# 		rows = cursor.fetchall()
-# 		return render_template('products.html', products=rows)
-# 	except Exception as e:
-# 		print(e)
-# 	finally:
-# 		cursor.close() 
-# 		conn.close()
+# @app.route('/products')
+def products():
+	try:
+		conn = db.connect()
+		cursor = conn.cursor(db.cursors.DictCursor)
+		cursor.execute("SELECT * FROM product")
+		rows = cursor.fetchall()
+		return render_template('products.html', products=rows)
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
 
 # @app.route('/empty')
-# def empty_cart():
-# 	try:
-# 		session.clear()
-# 		return redirect(url_for('.products'))
-# 	except Exception as e:
-# 		print(e)
+def empty_cart():
+	try:
+		session.clear()
+		return redirect(url_for('.products'))
+	except Exception as e:
+		print(e)
 
 # @app.route('/delete/<string:code>')
-# def delete_product(code):
-# 	try:
-# 		all_total_price = 0
-# 		all_total_quantity = 0
-# 		session.modified = True
+def delete_product(code):
+	try:
+		all_total_price = 0
+		all_total_quantity = 0
+		session.modified = True
 		
-# 		for item in session['cart_item'].items():
-# 			if item[0] == code:				
-# 				session['cart_item'].pop(item[0], None)
-# 				if 'cart_item' in session:
-# 					for key, value in session['cart_item'].items():
-# 						individual_quantity = int(session['cart_item'][key]['quantity'])
-# 						individual_price = float(session['cart_item'][key]['total_price'])
-# 						all_total_quantity = all_total_quantity + individual_quantity
-# 						all_total_price = all_total_price + individual_price
-# 				break
+		for item in session['cart_item'].items():
+			if item[0] == code:				
+				session['cart_item'].pop(item[0], None)
+				if 'cart_item' in session:
+					for key, value in session['cart_item'].items():
+						individual_quantity = int(session['cart_item'][key]['quantity'])
+						individual_price = float(session['cart_item'][key]['total_price'])
+						all_total_quantity = all_total_quantity + individual_quantity
+						all_total_price = all_total_price + individual_price
+				break
 		
-# 		if all_total_quantity == 0:
-# 			session.clear()
-# 		else:
-# 			session['all_total_quantity'] = all_total_quantity
-# 			session['all_total_price'] = all_total_price
+		if all_total_quantity == 0:
+			session.clear()
+		else:
+			session['all_total_quantity'] = all_total_quantity
+			session['all_total_price'] = all_total_price
 		
-# 		#return redirect('/')
-# 		return redirect(url_for('.products'))
-# 	except Exception as e:
-# 		print(e)
+		#return redirect('/')
+		return redirect(url_for('.products'))
+	except Exception as e:
+		print(e)
 		
 # def array_merge( first_array , second_array ):
 # 	if isinstance( first_array , list ) and isinstance( second_array , list ):
